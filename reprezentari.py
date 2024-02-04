5befd08c6d4503d9285f09e8e0ac8d401a2f12d9
timo170 <timotei.lni@gmail.com>
2024-02-04 18:35:15 +0200

    convertire
    stabilim daca ceea ce citim este matrice de adiacenta sau nu

--- reprezentari.py
+++ reprezentari.py
@@ -7,7 +7,8 @@ import config
 
 
 def reprezentari(type):
-    poz=config.poz + 1    # determinarea poziției pe ecran
+    config.poz=config.poz+1
+    poz=config.poz    # determinarea poziției pe ecran
     tip_graf=  config.graf[0][:len(config.graf[0])-1]
     space_poz= config.graf[1].find(' ')
     nr_varfuri = int(config.graf[1][:space_poz])
